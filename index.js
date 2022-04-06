3700dedf72cfc6dce4944a3bfee3197922d7ff37
Alex McDaniel <alexpmcdaniel@gmail.com>
2022-04-06 13:01:54 -0400

    commit tree 9d46b237ef8a7458b31e676dbf07b85a1e0f8a92

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+function findMatching(nameArray, oneName){
+    return nameArray.filter(name => name.toLowerCase() === oneName.toLowerCase())
+}
+function fuzzyMatch(nameArray, partialName){
+    return nameArray.filter(name => name.slice(0,partialName.length) === partialName)
+}
+function matchName(driverObjArray, nameMatch){
+    return driverObjArray.filter(driverObj => driverObj.name === nameMatch)
+}
